c5411a2625164d01c5b4301c936d76ced2378340
gombost84 <tamas.gombos84@gmail.com>
2022-01-08 16:20:34 +0100

    Java practices.

---
+++ src/main/java/methods.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+
+import java.util.Scanner;
+
+public class methods {
+    static String getName() {
+        Scanner nameInput = new Scanner(System.in); 
+        System.out.print("What is your name? ");
+        String myName = nameInput.next();
+        return myName;
+    }
+    static int getAge() {
+        Scanner ageInput = new Scanner(System.in);
+        System.out.print("What is your age? ");
+        int myAge = ageInput.nextInt();
+        return myAge;
+    }
+    static void myFirstMethod() {
+        System.out.println("Hello World from inside a method!");
+    }
+    static void mySecondMethod(String name, int age) {
+        System.out.println("Hello " + name + "! " + "You are " + age + " years old.");
+    }
+    static int myThirdMethod(int x) {
+        return x * x;
+    }
+
+public static void main(String[] args) {
+    
+    String inputName = getName();
+    int inputAge = getAge();
+    
+    myFirstMethod();
+    mySecondMethod(inputName, inputAge);
+    int z = myThirdMethod(inputAge);
+    System.out.printf("The square of %d is: " + z, inputAge);
+}
+    
+}
---
+++ src/main/java/variables.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class variables {
+    public static void main(String[] args) {
+        String name = "Tamas";
+        System.out.println("My name is: " + name);
+        
+        int age = 37;
+        System.out.println("My age is: " + age);
+        
+        age = 38;
+        System.out.println("I will soon be " + age);
+    }
+}
