d8152c01bb3c67bf701f2908f1ed0f0c51a1dd5e
gombost84 <tamas.gombos84@gmail.com>
2022-06-07 18:14:21 +0200

    Experimenting with different layouts.

--- gui/FirstGui/src/FirstGui.java
+++ gui/FirstGui/src/FirstGui.java
@@ -2,9 +2,6 @@ public class FirstGui {
     public static void main(String[] args) {
 
         FrameConstructor frame = new FrameConstructor();
-
-        frame.setFrame();
-        frame.setButton();
         frame.Frame();
     }
 }
--- gui/FirstGui/src/FrameConstructor.java
+++ gui/FirstGui/src/FrameConstructor.java
@@ -1,46 +1,41 @@
+import java.awt.BorderLayout;
 import javax.swing.*;
 
 public class FrameConstructor {
 
-    private int frameWidth;
-    private int frameHeight;
-    private int buttonWidth;
-    private int buttonHeight;
-    private int buttonX;
-    private int buttonY;
-    private String buttonText;  
-    
-    public void setFrame() {
-        String width = JOptionPane.showInputDialog("Frame width: ");
-        String height = JOptionPane.showInputDialog("Frame height: ");
-
-        this.frameWidth = Integer.parseInt(width);
-        this.frameHeight = Integer.parseInt(height);
-    }
-
-    public void setButton() {
-        String x = JOptionPane.showInputDialog("Button x: ");
-        String y = JOptionPane.showInputDialog("Button y: ");
-        String width = JOptionPane.showInputDialog("Button width: ");
-        String height = JOptionPane.showInputDialog("Button height: ");
-        String text = JOptionPane.showInputDialog("Button text: ");
-
-        this.buttonX = Integer.parseInt(x);
-        this.buttonY = Integer.parseInt(y);
-        this.buttonWidth = Integer.parseInt(width);
-        this.buttonHeight = Integer.parseInt(height);
-        this.buttonText = text;
-    }
+    private int frameWidth = 800;
+    private int frameHeight = 600;
 
     public void Frame() {
         JFrame frame = new JFrame();
-        JButton button = new JButton(buttonText);
 
-        frame.setSize(frameWidth, frameHeight);
-        button.setBounds(buttonX, buttonY, buttonWidth, buttonHeight);
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
 
-        frame.add(button);
-        frame.setLayout(null);
+        JButton button1 = new JButton("Button 1");
+        JButton button2 = new JButton("Button 2");
+        JButton button3 = new JButton("Button 3");
+        JButton button4 = new JButton("Button 4");        
+        JButton button5 = new JButton("Button 5");
+
+        JTextArea textArea = new JTextArea(20, 20);
+        
+
+        frame.setSize(frameWidth, frameHeight);        
+        frame.setLayout(new BorderLayout());
+        frame.getContentPane().add(panel1, BorderLayout.PAGE_START);
+        frame.getContentPane().add(panel2, BorderLayout.CENTER);
+
+        panel1.setLayout(new BorderLayout());
+
+        panel1.add(button1, BorderLayout.PAGE_START);
+        panel1.add(button2, BorderLayout.LINE_START);
+        panel1.add(button3, BorderLayout.CENTER);
+        panel1.add(button4, BorderLayout.LINE_END);
+        panel1.add(button5, BorderLayout.PAGE_END);
+
+        panel2.add(textArea);
+
         frame.setVisible(true);
     }
     
