f90431b5eab22c470c2233cc15e8f4eac5885a15
gombost84 <tamas.gombos84@gmail.com>
2022-06-08 17:38:17 +0200

    Refactoring

--- gui/FirstGui/src/FirstGui.java
+++ gui/FirstGui/src/FirstGui.java
@@ -2,6 +2,7 @@ public class FirstGui {
     public static void main(String[] args) {
 
         FrameConstructor frame = new FrameConstructor();
+
         frame.Frame();
     }
 }
--- gui/FirstGui/src/FrameConstructor.java
+++ gui/FirstGui/src/FrameConstructor.java
@@ -1,22 +1,39 @@
 import java.awt.BorderLayout;
 import javax.swing.*;
 
-public class FrameConstructor {
+class FrameConstructor {
 
     private int frameWidth = 800;
     private int frameHeight = 600;
 
     public void Frame() {
+        BuildFrame();
+    }  
+
+    private JButton Button(String text) {
+        JButton button = new JButton(text);
+
+        return button;
+    }
+
+    private JPanel Panel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        return panel;
+    }  
+
+    private void BuildFrame() {
         JFrame frame = new JFrame();
 
-        JPanel panel1 = new JPanel();
-        JPanel panel2 = new JPanel();
+        JPanel panel1 = Panel();       
+        JPanel panel2 = Panel();
 
-        JButton button1 = new JButton("Button 1");
-        JButton button2 = new JButton("Button 2");
-        JButton button3 = new JButton("Button 3");
-        JButton button4 = new JButton("Button 4");        
-        JButton button5 = new JButton("Button 5");
+        JButton button1 = Button("Button 1");
+        JButton button2 = Button("Button 2");
+        JButton button3 = Button("Button 3");
+        JButton button4 = Button("Button 4");        
+        JButton button5 = Button("Button 5");
 
         JTextArea textArea = new JTextArea(20, 20);
         
@@ -24,9 +41,7 @@ public class FrameConstructor {
         frame.setSize(frameWidth, frameHeight);        
         frame.setLayout(new BorderLayout());
         frame.getContentPane().add(panel1, BorderLayout.PAGE_START);
-        frame.getContentPane().add(panel2, BorderLayout.CENTER);
-
-        panel1.setLayout(new BorderLayout());
+        frame.getContentPane().add(panel2, BorderLayout.CENTER);        
 
         panel1.add(button1, BorderLayout.PAGE_START);
         panel1.add(button2, BorderLayout.LINE_START);
@@ -37,6 +52,7 @@ public class FrameConstructor {
         panel2.add(textArea);
 
         frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
     
 }
