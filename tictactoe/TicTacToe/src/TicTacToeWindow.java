718541e0267a102c3fc8f4b3c72bbd9875af1393
gombost84 <tamas.gombos84@gmail.com>
2022-11-17 13:36:05 +0100

    Updates to datastructures, now using fancy arrays.

--- tictactoe/TicTacToe/src/TicTacToeWindow.java
+++ tictactoe/TicTacToe/src/TicTacToeWindow.java
@@ -13,27 +13,20 @@ public class TicTacToeWindow implements ActionListener {
         private int windowWidth = 600;
         private int windowHeight = 800;
         private int menuHeight = 20;
-        private String name = "Tic Tac Toe";
-        private String lastSymbol = "";
-        private String button1Text = "";
-        private String button2Text = "";
-        private String button3Text = "";
-        private String button4Text = "";
-        private String button5Text = "";
-        private String button6Text = "";
-        private String button7Text = "";
-        private String button8Text = "";
-        private String button9Text = "";
-
-        private JButton button1 = TicTacToeButton();
-        private JButton button2 = TicTacToeButton();
-        private JButton button3 = TicTacToeButton();
-        private JButton button4 = TicTacToeButton();
-        private JButton button5 = TicTacToeButton();
-        private JButton button6 = TicTacToeButton();
-        private JButton button7 = TicTacToeButton();
-        private JButton button8 = TicTacToeButton();
-        private JButton button9 = TicTacToeButton();
+        private String name = "Tic Tac Toe";        
+        private String[] buttonValues = {
+            "", // value at index 0 is the last used symbol
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
+        };
+        private JButton[] buttonList = TicTacToeButton();
 
         TicTacToeWindow() {
         
@@ -44,27 +37,20 @@ public class TicTacToeWindow implements ActionListener {
     private JFrame GameWindow(JFrame frame, JMenuBar menu) {
         
         frame.setJMenuBar(menu);
-        frame.add(button1);
-        frame.add(button2);
-        frame.add(button3);
 
-        frame.add(button4);
-        frame.add(button5);
-        frame.add(button6);
-
-        frame.add(button7);
-        frame.add(button8);
-        frame.add(button9);
+        for (JButton jButton : buttonList) {
+            frame.add(jButton);
+        }
                
-        button1.addActionListener(this);                
-        button2.addActionListener(this);                
-        button3.addActionListener(this);
-        button4.addActionListener(this);
-        button5.addActionListener(this);
-        button6.addActionListener(this);
-        button7.addActionListener(this);
-        button8.addActionListener(this);
-        button9.addActionListener(this);
+        buttonList[0].addActionListener(this);                
+        buttonList[1].addActionListener(this);                
+        buttonList[2].addActionListener(this);
+        buttonList[3].addActionListener(this);
+        buttonList[4].addActionListener(this);
+        buttonList[5].addActionListener(this);
+        buttonList[6].addActionListener(this);
+        buttonList[7].addActionListener(this);
+        buttonList[8].addActionListener(this);
 
         return frame;
     }
@@ -93,35 +79,55 @@ public class TicTacToeWindow implements ActionListener {
         return menuBar;
     }
 
-    private JButton TicTacToeButton() {
+    private JButton[] TicTacToeButton() {
+        
+        JButton button1 = new JButton();
+        JButton button2 = new JButton();
+        JButton button3 = new JButton();
+        JButton button4 = new JButton();
+        JButton button5 = new JButton();
+        JButton button6 = new JButton();
+        JButton button7 = new JButton();
+        JButton button8 = new JButton();
+        JButton button9 = new JButton();
 
-        JButton button = new JButton("Click me");
+        JButton[] array = {
+            button1,
+            button2,
+            button3,
+            button4,
+            button5,
+            button6,
+            button7,
+            button8,
+            button9
+        };
 
-        return button;
+        return array;
     }
 
-    private void buttonAction(JButton button, String buttonText) {
-        switch (lastSymbol) {
+    private void buttonAction(JButton button, int index, String[] array) {
+        switch (array[0]) {
             
             case "":
-                buttonText = "X";
-                button.setText(buttonText);
-                lastSymbol = buttonText;
+                array[index] = "X";
+                button.setText(array[index]);
+                array[0] = array[index];
 
             case "X":
-                switch (buttonText) {
+                switch (array[index]) {
                     case "":
-                        buttonText = "O";
-                        button.setText(buttonText);
-                        lastSymbol = buttonText;
+                        array[index] = "O";
+                        button.setText(array[index]);
+                        array[0] = array[index];
                 }
 
             case "O":
-                switch (buttonText) {
+                switch (array[index]) {
                     case "":
-                        buttonText = "X";
-                        button.setText(buttonText);
-                        lastSymbol = buttonText;
+                        array[index] = "X";
+                        button.setText(array[index]);
+                        array[0] = array[index];
             }
         }
 
@@ -130,36 +136,24 @@ public class TicTacToeWindow implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         
-        if (e.getSource() == button1) {
-            buttonAction(button1, button1Text);
-        } else if (e.getSource() == button2) {
-            buttonAction(button2, button2Text);                        
-        } else if (e.getSource() == button3) {
-            buttonAction(button3, button3Text);            
-        } else if (e.getSource() == button4) {
-            buttonAction(button4, button4Text);            
-        } else if (e.getSource() == button5) {
-            buttonAction(button5, button5Text);            
-        } else if (e.getSource() == button6) {
-            buttonAction(button6, button6Text);            
-        } else if (e.getSource() == button7) {
-            buttonAction(button7, button7Text);            
-        } else if (e.getSource() == button8) {
-            buttonAction(button8, button8Text);            
-        } else if (e.getSource() == button9) {
-            buttonAction(button9, button9Text);            
+        if (e.getSource() == buttonList[0]) {
+            buttonAction(buttonList[0], 1, buttonValues);
+        } else if (e.getSource() == buttonList[1]) {
+            buttonAction(buttonList[1], 2, buttonValues);                        
+        } else if (e.getSource() == buttonList[2]) {
+            buttonAction(buttonList[2], 3, buttonValues);            
+        } else if (e.getSource() == buttonList[3]) {
+            buttonAction(buttonList[3], 4, buttonValues);            
+        } else if (e.getSource() == buttonList[4]) {
+            buttonAction(buttonList[4], 5, buttonValues);            
+        } else if (e.getSource() == buttonList[5]) {
+            buttonAction(buttonList[5], 6, buttonValues);            
+        } else if (e.getSource() == buttonList[6]) {
+            buttonAction(buttonList[6], 7, buttonValues);            
+        } else if (e.getSource() == buttonList[7]) {
+            buttonAction(buttonList[7], 8, buttonValues);            
+        } else if (e.getSource() == buttonList[8]) {
+            buttonAction(buttonList[8], 9, buttonValues);            
         }
-
-        // switch (source) {
-        //     case "button1": buttonAction(button1, button1Text);
-        //     case "button2": buttonAction(button2, button2Text);
-        //     case "button3": buttonAction(button3, button3Text);
-        //     case "button4": buttonAction(button4, button4Text);
-        //     case "button5": buttonAction(button5, button5Text);
-        //     case "button6": buttonAction(button6, button6Text);
-        //     case "button7": buttonAction(button7, button7Text);
-        //     case "button8": buttonAction(button8, button8Text);
-        //     case "button9": buttonAction(button9, button9Text);
-        // }
     }
 }
