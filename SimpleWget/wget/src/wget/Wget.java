197e6c09fe437d35e1d29e84ebcead5a070e43c5
gombost84 <tamas.gombos84@gmail.com>
2023-11-18 15:42:56 +0100

    Forgot to commit as well.

---
+++ SimpleWget/wget/src/wget/Wget.java
@@ -0,0 +1,16 @@
+package wget;
+import In.java;
+/**
+ * @author https://github.com/gombost84
+ * Reads in a URL specified on the command line and saves its contents
+ * in a file with the given name.
+ */
+public class Wget {
+    
+    public static void main(String[] args) {
+        String url = args[0];
+        In in = new System.In(url);
+        
+    }
+    
+}
